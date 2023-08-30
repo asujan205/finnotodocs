d97eccba8d53d99db3fe27ea95f5a52f23651de3
asujan205 <asujan205@gmail.com>
2023-08-30 11:16:04 +0545

    btn

--- pages/login/login.tsx
+++ pages/login/login.tsx
@@ -53,8 +53,14 @@ const SigninPage = () => {
     <div>
       <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <img
+            className="mx-auto h-12 w-auto cursor-pointer"
+            src="/logo.png"
+            alt="logo"
+            onClick={() => router.push("/")}
+          />
           <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            SignIn
+            Sign in to your account
           </h2>
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
@@ -103,9 +109,9 @@ const SigninPage = () => {
                     onClick={handleSubmit}
                     disabled={isLoading}
                     className={cn(
-                      "flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm",
+                      "flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm ",
                       "focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
-                      "bg-indigo-600 text-white hover:bg-indigo-500 ",
+                      "bg-indigo-600  hover:bg-indigo-500 ",
                       "disabled:cursor-not-allowed disabled:bg-indigo-300 disabled:opacity-50"
                     )}
                   >
