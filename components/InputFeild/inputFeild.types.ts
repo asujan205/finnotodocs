43eaa7bf77c449a5d0f0bf3c2b8a938374edd917
asujan205 <asujan205@gmail.com>
2023-08-30 17:12:18 +0545

    #input level

--- components/InputFeild/inputFeild.types.ts
+++ components/InputFeild/inputFeild.types.ts
@@ -1,1 +1,18 @@
-export interface InputFeildProps {}
+export interface InputFeildProps {
+  type?: "email" | "password";
+
+  onChange?(value: any): void;
+  value?: string;
+  placeholder?: string;
+  name?: string;
+  error?: string;
+  className?: string;
+  disabled?: boolean;
+  required?: boolean;
+}
+
+export interface LabelProps {
+  children?: React.ReactNode;
+  className?: string;
+  htmlFor?: string;
+}
---
+++ components/InputFeild/inputField.tsx
@@ -0,0 +1,29 @@
+import { InputFeildProps } from "./inputFeild.types";
+import { LabelProps } from "./inputFeild.types";
+
+const InputFeild = (props: InputFeildProps) => {
+  return (
+    <div className="input-container">
+      <input
+        type={props.type}
+        name={props.name}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.onChange}
+        className={props.className}
+        required={props.required}
+      />
+      {props.error && <p className="error">{props.error}</p>}
+    </div>
+  );
+};
+
+export default InputFeild;
+
+export const Label = (props: LabelProps) => {
+  return (
+    <label htmlFor={props.htmlFor} className={props.className}>
+      {props.children}
+    </label>
+  );
+};
