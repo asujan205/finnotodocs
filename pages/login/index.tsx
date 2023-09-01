90c869152b0f1a1c150463dc4e3634ee76ed9954
asujan205 <asujan205@gmail.com>
2023-09-01 19:50:56 +0545

    #changes

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -5,14 +5,12 @@ import LoginForm from "../../components/Form/login.form";
 const SigninPage = () => {
   return (
     <div className="bg-white w-screen h-screen inset-y-0 absolute ">
-      <div className="	">
-        <AuthWrapper>
-          <IconWrapper />
-          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-            <LoginForm />
-          </div>
-        </AuthWrapper>
-      </div>
+      <AuthWrapper>
+        <IconWrapper />
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <LoginForm />
+        </div>
+      </AuthWrapper>
     </div>
   );
 };
