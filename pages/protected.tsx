7342d95bd090f549b60cb8241209165d6b1f677c
asujan205 <asujan205@gmail.com>
2023-09-01 15:48:39 +0545

    ##login

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -1,13 +1,16 @@
 import { useAuth } from "../utils/provider/authContext";
 import { useRouter } from "next/router";
 import SigninPage from "./login";
+import { useEffect } from "react";
 
 const ProtectedPage = ({ children }: any) => {
   const { isAuthenticated } = useAuth();
   const router = useRouter();
-  if (!isAuthenticated && router.pathname.includes("/dashboard")) {
-    return <SigninPage />;
-  }
+  useEffect(() => {
+    if (!isAuthenticated && router.pathname.includes("/dashboard")) {
+      router.push("/login");
+    }
+  }, [isAuthenticated, router.pathname]);
 
   return children;
 };
