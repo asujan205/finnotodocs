37a7ec54fed5a161835eb2fad2f46a26baeff5ea
asujan205 <asujan205@gmail.com>
2023-09-01 10:12:25 +0545

    #BlogComponent

---
+++ components/Blog/BlogHeader.tsx
@@ -0,0 +1,9 @@
+export function BlogHeader() {
+  return (
+    <div className="max-w-screen-lg mx-auto pt-4 pb-8 mb-16 border-b border-gray-400 border-opacity-20 flex flex-row justify-between items-center">
+      <p className="text-center text-gray-500 dark:text-gray-400 font-space-grotesk mt-2">
+        Get In Touch with What Finnoto Teams says.
+      </p>
+    </div>
+  );
+}
