847048a49901b7716d0b06f574342ff67c507879
asujan205 <asujan205@gmail.com>
2023-09-01 11:13:56 +0545

    loginBtn

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,5 +1,49 @@
 import { ButtonProps } from "./Button.types";
+import { cn } from "../../utils/common.ui.utils";
+const Button: React.FC<ButtonProps> = ({
+  type = "button",
+  children,
+  className,
+  onClick,
+  disabled,
+  apperance,
+  loading,
+}) => {
+  const getButtonClassNames = () => {
+    if (apperance === "login") {
+      return cn(
+        "flex w-full justify-center px-3 py-2 text-sm font-semibold leading-6 shadow-sm",
+        "focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
+        "bg-black text-white hover:bg-black hover:text-white",
+        "disabled:cursor-not-allowed disabled:bg-indigo-300 disabled:opacity-50",
+        className
+      );
+    } else if (apperance === "cancel") {
+      return cn(
+        "flex w-full justify-center px-3 py-2 text-sm font-semibold leading-6 shadow-sm",
+        className
+      );
+    } else {
+      // Default button styles
+      return cn(
+        "px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm",
+        "hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500",
+        "disabled:opacity-50 disabled:cursor-not-allowed",
+        className
+      );
+    }
+  };
 
-export const Button = ({ children, ...props }: ButtonProps) => {
-  return <button {...props}>{children}</button>;
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled || loading}
+      className={getButtonClassNames()}
+    >
+      {loading ? "Loading..." : children}
+    </button>
+  );
 };
+
+export default Button;
--- pages/login/login.tsx
+++ pages/login/login.tsx
@@ -1,3 +1,134 @@
-import SigninPage from "../../utils/Modules/login.modules";
+import { useState } from "react";
+import { cn } from "../../utils/common.ui.utils";
+import Cookies from "js-cookie";
+
+import { useRouter } from "next/router";
+import { useAuth } from "../../utils/provider/authContext";
+import axios from "axios";
+import InputFeild from "../../components/InputFeild/inputField";
+import Button from "../../components/Button/Button";
+
+const SigninPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { setIsAuthenticated } = useAuth();
+  const router = useRouter();
+  const [username, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.SyntheticEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const response = await axios.post(
+        "https://meta.finnoto.dev/auth/login",
+        {
+          username,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status === 201) {
+        Cookies.set("token", response.data.user.access_token);
+
+        router.push("/");
+        setIsAuthenticated(true);
+        setIsLoading(false);
+      } else {
+        setIsLoading(false);
+        console.log(response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="bg-white w-screen h-screen">
+      <div className="flex flex-col mx-auto items-center justify-between min-h-[500px] max-w-[500px] shadow-md rounded-sm py-12 my-auto sm:w-full">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <img
+            className="mx-auto h-12 w-auto"
+            src="/logo-1.png"
+            alt="logo"
+            onClick={() => router.push("/")}
+          />
+          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign In
+          </h2>
+          <p className="mt-2 text-center text-sm leading-5 text-gray-600 max-w">
+            Enter yours credentials to access your account
+          </p>
+        </div>
+
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <form className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="password"
+                  required
+                  className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="mt-6">
+                <Button
+                  type="submit"
+                  apperance="login"
+                  onClick={handleSubmit}
+                  loading={isLoading}
+                  disabled={isLoading}
+                >
+                  {!isLoading ? "Log in" : "Login in..."}
+                </Button>
+
+                <Button
+                  type="button"
+                  apperance="cancel"
+                  onClick={() => router.push("/")}
+                >
+                  Cancel
+                </Button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SigninPage;
